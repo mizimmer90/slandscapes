f6fb627e56465b33a25e4571707742c0b0e7ffd2
Mickey Ward <mickward1029@gmail.com>
2017-11-28 10:00:52 -0600

    Added sigmoid scaling

--- scalings.py
+++ scalings.py
@@ -12,3 +12,19 @@ class feature_scale:
         else:
             scaled_values = (values.max() - values) / value_spread
         return scaled_values
+
+class sigmoid_scale:
+    def __init__(self, maximize=True,a=3,b=2):
+        self.maximize = maximize
+        self.a = a
+        self.b = b
+    def scale(self, values):
+        sigma = np.median(values)
+        if self.maximize:
+            scaled_values = (1 - (1+(((2**(self.a/self.b)) - 1)*((values/sigma)**self.a)))**(-self.b/self.a))
+        else:
+            scaled_values = 1-(1 - (1+(((2**(self.a/self.b)) - 1)*((values/sigma)**self.a)))**(-self.b/self.a))
+        return scaled_values
+
+
+
